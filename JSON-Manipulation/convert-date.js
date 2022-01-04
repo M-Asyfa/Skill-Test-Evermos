a872534032c5d8ec9eb8db2db24b50150f29c474
Muhammad Asyfa Dwi Adzkiya <muhammad.asyfa.1995@gmail.com>
2022-01-04 10:40:26 +0700

    first commit

---
+++ JSON-Manipulation/convert-date.js
@@ -0,0 +1,14 @@
+date1 = '1579190471';
+date2 = '1579190642';
+date3 = '1579017842';
+date4 = '1578931442';
+date5 = '1578672242';
+
+// const myString = Date.parse('16 Januari 2020');
+// const myString = Date.parse('2020-01-16');
+// const myString = Date.parse('January 16, 2020');
+const myString = Date.parse('1/16/2020');
+// const myDate = string.parse('1579190471');
+
+console.log(myString);
+// console.log(myDate);
